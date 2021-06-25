2e4b50d64c8ebbc56c2c105bc3e71fab8cb94b8a
Adolfo Carneiro <70848687+AdolfoCarneiro@users.noreply.github.com>
2021-06-25 15:44:48 -0300

    Finalizado
    Projeto finalizado com a adição de uma tela de fim e armazenamento dos votos em uma variável

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
@@ -95,17 +96,24 @@ function confirma(){
     let votoConfirmado = false;
     if(votoBranco === true){
         votoConfirmado = true;
-        console.log("VOCE VOTOU EM BRANCO");
+        votos.push({
+            etapa:etapas[etapaAtual].titulo,
+            voto:'branco',
+        })
     }else if(numero.length === etapa.numeros){
         votoConfirmado = true;
-        console.log("VOCE VOTOU EM "+numero);
+        votos.push({
+            etapa:etapas[etapaAtual].titulo,
+            voto:numero,
+        })
     }
     if(votoConfirmado){
         etapaAtual++;
         if(etapas[etapaAtual] !== undefined){
             comecarEtapa();
         }else{
-            console.log("fim");
+            document.querySelector('.tela').innerHTML = `<div class="aviso--gigante">FIM<div/>`;
+            console.log(votos);
         }
     }
 }
